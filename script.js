da6d9b90404be32f102fed3f8a974b1c45ef92cf
= <abbbbbbbbbbbhi@gmail.com>
2019-10-19 13:44:42 -0700

    sad

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
+$("#search").on("click", function (){
+    let yourkey = "o3kvEj2HXoHJy8nusb7wrgGQmZPtY1i1"
+    //$("#searchterm").val
+    let search = $("#searchterm").val();
+    let num = $("#retrieve").val();
+    let startyear =  $("#startyear").val();
+    let endyear =  $("#endyear").val();
+
+    console.log(search + num + startyear + endyear);
+
+    let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${yourkey}`
 
 
-let yourkey = "o3kvEj2HXoHJy8nusb7wrgGQmZPtY1i1"
-//$("#searchterm").val
-let search = "election"
-//let num = //
-let startyear = $("#searchterm").val
-let endyear;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
 
-let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${yourkey}`
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-
-    console.log(response);
-    console.log(response.response.docs);
-    response.response.docs.forEach(element => {
-        console.log(element.headline.main);
-        console.log("--" + element.byline.original)
-        
-        
-        
+        console.log(response);
+        console.log(response.response.docs);
+        response.response.docs.forEach(element => {
+            console.log(element.headline.main);
+            console.log("--" + element.byline.original)
+            
+            
+            
+        });
     });
-  });
+});
