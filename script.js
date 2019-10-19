fa06f6439b9d04e959939932ae44c18f444caea4
= <abbbbbbbbbbbhi@gmail.com>
2019-10-19 13:30:41 -0700

    stuff

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
 let yourkey = "o3kvEj2HXoHJy8nusb7wrgGQmZPtY1i1"
+//$("#searchterm").val
 let search = "election"
 let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${yourkey}`
 
@@ -9,10 +10,12 @@ $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
+
     console.log(response);
     console.log(response.response.docs);
     response.response.docs.forEach(element => {
         console.log(element.headline.main);
+        console.log("--" + element.byline.original)
         
         
     });
