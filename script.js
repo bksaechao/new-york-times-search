61bd53ad547845ab37ae99cb4908c35d1463eafb
= <abbbbbbbbbbbhi@gmail.com>
2019-10-19 13:19:21 -0700

    added ajax in script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+let yourkey = "o3kvEj2HXoHJy8nusb7wrgGQmZPtY1i1"
+let search = "election"
+let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${yourkey}`
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    console.log(response.response.docs);
+    response.response.docs.forEach(element => {
+        console.log(element.headline.main);
+        
+        
+    });
+  });
